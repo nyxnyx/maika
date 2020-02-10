ea114d454bab0802c87fbac03c513305dc1b74c5
Grzegorz Szostak <szostak.grzegorz@gmail.com>
2020-02-10 23:12:03 +0100

    rename sensors

--- __init__.py
+++ __init__.py
@@ -73,16 +73,16 @@ class AikaData(object):
         self._status = None
         self.SENSOR_TYPES = {
             'maika.battery'      : ['Battery', None, 'mdi:battery'],
-            'maika.batteryStatus': ['Battery Status', None, 'mdi:battery'],
+            'maika.batterystatus': ['Battery Status', None, 'mdi:battery'],
             'maika.course'       : ['Course', None, 'mdi:compass'],
-            'maika.dataContext'  : ['Data Context', None, 'mdi:message-text'],
-            'maika.deviceID'     : ['Device ID', None, None],
-            'maika.deviceName'   : ['Device Name', None, 'mdi:rename-box'],
-            'maika.ICCID'        : ['ICCID', None, None],
+            'maika.datacontext'  : ['Data Context', None, 'mdi:message-text'],
+            'maika.deviceid'     : ['Device ID', None, None],
+            'maika.devicename'   : ['Device Name', None, 'mdi:rename-box'],
+            'maika.iccid'        : ['ICCID', None, None],
             'maika.icon'         : ['Icon', None, None],
             'maika.id'           : ['ID', None, None],
-            'maika.isGPS'        : ['Is GPS', None, 'mdi:crosshairs-gps'],
-            'maika.isStop'       : ['Is Stop', None, 'mdi:car'],
+            'maika.isgps'        : ['Is GPS', None, 'mdi:crosshairs-gps'],
+            'maika.isstop'       : ['Is Stop', None, 'mdi:car'],
             #'maika.key2018'     : ['', None, None],
             'maika.lat'          : ['Latitude', None, 'mdi:latitude'],
             'maika.lng'          : ['Longitude', None, 'mdi:longitude'],
@@ -91,24 +91,24 @@ class AikaData(object):
             'maika.ofl'          : ['Ofl', None, None],
             'maika.olat'         : ['Old latitude', None, 'mdi:latitude'],
             'maika.olng'         : ['Old longitude', None, 'mdi:longitude'],
-            'maika.positionTime' : ['Position time', None, 'mdi:clock-outline'],
+            'maika.positiontime' : ['Position time', None, 'mdi:clock-outline'],
             #'maika.serialNumber' : ['Serial Number', None, None],
-            'maika.sendCommand'  : ['Send Command', None, 'mdi:message-text'],
+            'maika.sendcommand'  : ['Send Command', None, 'mdi:message-text'],
             'maika.speed'        : ['Speed', None, 'mdi:speedometer'],
             'maika.sn'           : ['SN', None, 'mdi:message-text'],
-            'maika.serialNumber' : ['Serial Number', None, 'mdi:message-text'],
-            'maika.sendCommand'  : ['Send Command', None, 'mdi:message-text'],
+            'maika.serialnumber' : ['Serial Number', None, 'mdi:message-text'],
+            'maika.sendcommand'  : ['Send Command', None, 'mdi:message-text'],
             'maika.state'        : ['State', None, 'mdi:message-text'],
             'maika.status'       : ['Status', None, 'mdi:message-text'],
-            'maika.statusX20'    : ['Status X20', None, 'mdi:message-text'],
+            'maika.statusx20'    : ['Status X20', None, 'mdi:message-text'],
             'maika.stm'          : ['STM', None, 'mdi:message-text'],
-            'maika.timeZone'     : ['Time Zone', None, 'mdi:map-clock'],
-            'maika.VIN'          : ['VIN', None, 'mdi:message-text'],
+            'maika.timezone'     : ['Time Zone', None, 'mdi:map-clock'],
+            'maika.vin'          : ['VIN', None, 'mdi:message-text'],
             'maika.voice'        : ['Has Voice', None, 'mdi:account-voice'],
             'maika.warn'         : ['Warning', None, 'mdi:message-text'],
-            'maika.warnStr'      : ['Warning string', None, 'mdi:message-text'],
-            'maika.warnTime'     : ['Warning time', None, 'mdi:clock-outline'],
-            'maika.warnTxt'      : ['Warning text', None, 'mdi:message-text'],
+            'maika.warnstr'      : ['Warning string', None, 'mdi:message-text'],
+            'maika.warntime'     : ['Warning time', None, 'mdi:clock-outline'],
+            'maika.warntxt'      : ['Warning text', None, 'mdi:message-text'],
             'maika.work'         : ['Work', None, 'mdi:message-text'],
             'maika.xg'           : ['Xg', None, 'mdi:message-text'],
             'maika.yinshen'      : ['yinshen', None, 'mdi:message-text'],
@@ -138,16 +138,16 @@ class AikaData(object):
 
             v={}
             v['maika.battery']       = self.api.__getattribute__('battery')
-            v['maika.batteryStatus'] = self.api.__getattribute__('batteryStatus')
+            v['maika.batterystatus'] = self.api.__getattribute__('batteryStatus')
             v['maika.course']        = self.api.__getattribute__('course')
-            v['maika.dataContext']   = self.api.__getattribute__('dataContext')
-            v['maika.deviceID']      = self.api.__getattribute__('deviceID')
-            v['maika.deviceName']    = self.api.__getattribute__('deviceName')
-            v['maika.ICCID']         = self.api.__getattribute__('ICCID')
+            v['maika.datacontext']   = self.api.__getattribute__('dataContext')
+            v['maika.deviceid']      = self.api.__getattribute__('deviceID')
+            v['maika.devicename']    = self.api.__getattribute__('deviceName')
+            v['maika.iccid']         = self.api.__getattribute__('ICCID')
             v['maika.icon']          = self.api.__getattribute__('icon')
             v['maika.id']            = self.api.__getattribute__('id')
-            v['maika.isGPS']         = self.api.__getattribute__('isGPS')
-            v['maika.isStop']        = self.api.__getattribute__('isStop')
+            v['maika.isgps']         = self.api.__getattribute__('isGPS')
+            v['maika.isstop']        = self.api.__getattribute__('isStop')
             #v['maika.key2018']       = self.api.__getattribute__('key2018')
             v['maika.lat']           = self.api.__getattribute__('lat')
             v['maika.lng']           = self.api.__getattribute__('lng')
@@ -156,22 +156,22 @@ class AikaData(object):
             v['maika.ofl']           = self.api.__getattribute__('ofl')
             v['maika.olat']          = self.api.__getattribute__('olat')
             v['maika.olng']          = self.api.__getattribute__('olng')
-            v['maika.positionTime']  = self.api.__getattribute__('positionTime')
+            v['maika.positiontime']  = self.api.__getattribute__('positionTime')
             #v['maika.serialNumber']  = self.api.__getattribute__('serialNumber')
-            v['maika.sendCommand']   = self.api.__getattribute__('sendCommand')
+            v['maika.sendcommand']   = self.api.__getattribute__('sendCommand')
             v['maika.speed']         = self.api.__getattribute__('speed')
             v['maika.sn']            = self.api.__getattribute__('sn')
             v['maika.state']         = self.api.__getattribute__('state')
             v['maika.status']        = self.api.__getattribute__('status')
-            v['maika.statusX20']     = self.api.__getattribute__('statusX20')
+            v['maika.statusx20']     = self.api.__getattribute__('statusX20')
             v['maika.stm']           = self.api.__getattribute__('stm')
-            v['maika.timeZone']      = self.api.__getattribute__('timeZone')
-            v['maika.VIN']           = self.api.__getattribute__('VIN')
+            v['maika.timezone']      = self.api.__getattribute__('timeZone')
+            v['maika.vin']           = self.api.__getattribute__('VIN')
             v['maika.voice']         = self.api.__getattribute__('voice')
             v['maika.warn']          = self.api.__getattribute__('warn')
-            v['maika.warnStr']       = self.api.__getattribute__('warnStr')
-            v['maika.warnTime']      = self.api.__getattribute__('warnTime')
-            v['maika.warnTxt']       = self.api.__getattribute__('warnTxt')
+            v['maika.warnstr']       = self.api.__getattribute__('warnStr')
+            v['maika.warntime']      = self.api.__getattribute__('warnTime')
+            v['maika.warntxt']       = self.api.__getattribute__('warnTxt')
             v['maika.work']          = self.api.__getattribute__('work')
             v['maika.xg']            = self.api.__getattribute__('xg')
             v['maika.yinshen']       = self.api.__getattribute__('yinshen')
